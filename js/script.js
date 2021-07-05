cbd0e392b5b12d8e541f34deefd025fc0f18fc33
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2021-07-05 14:26:52 -0400

    Steps 3 and 4 complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-console.log("Test!");
+// Test Code to ensure everything is set up correctly.
+// console.log("Test!");
+
+// Step 3: The Name Field. This code will cause cursor to automatically appear in the first text field.
+// The .focus() method will be used to achieve this. 
+
+const nameField = document.getElementById('name');
+nameField.focus();
+
+// Step 4: The Job Role Section. I will hide the Other Job Role input text by using style.display = 'none'.
+// I will then use a 'Change' event listener to pick up on the moments when a person selects 
+
+let otherJobRoleStatus = document.getElementById('other-job-role');
+otherJobRoleStatus.style.display = 'none';
+let jobRoleField = document.getElementById('title');
+
+jobRoleField.addEventListener('change', (e) => {
+    //console.log(e.target);
+    if ( e.target.value == "other" ) {
+        otherJobRoleStatus.style.display = 'block';
+    } else {
+        otherJobRoleStatus.style.display = 'none';
+    }
+})
+
+// Step 5: 
+
+
+
+
+
+
